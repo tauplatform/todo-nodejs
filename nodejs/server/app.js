c510fd8aa7441332bfaf42898a75b5aabc20c925
vmusulainen <vmusulainen@gmail.com>
2017-04-05 21:01:28 +0300

    adding partials and styles

--- nodejs/server/app.js
+++ nodejs/server/app.js
@@ -3,19 +3,23 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var hbs = require ("hbs");
 
 var models = require("./models");
-
-var serverPort = Rho.System.NodejsServerPort;
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var serverPort = Rho.System.NodejsServerPort;
+
 var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
